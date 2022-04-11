b8f51e66291d274533993d48ca8bbbd9c6f8df05
Karim Hraibi <khraibi@apple.com>
2022-04-11 09:40:31 +0200

    Added release script

---
+++ release.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Create a zip file containing the source code including submodules
+#
+# Usage: release <version>
+
+
+version="$1"
+file_name="smart_pump"
+
+if [[ -z $version ]]; then
+  echo "Error: No version string specified"
+  echo "Usage: $0 <version>"
+  exit 1
+fi
+
+rm -r "$file_name-"*"-full"*
+
+if [[ $version != *"clean"* ]]; then
+  zip -r "$file_name-$version-full.zip" . -x '*.git*' '*.vscode*'
+fi
+
+exit 0
