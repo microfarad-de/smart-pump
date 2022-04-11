924a38c3818dbec2fac663306ec6cff83d316a02
Karim Hraibi <khraibi@apple.com>
2022-04-11 14:16:26 +0200

    Removed commented code from release script

--- release.sh
+++ release.sh
@@ -12,16 +12,6 @@ version_maint=$(cat $version_file | grep VERSION_MAINT | awk -F' ' '{print $3}')
 
 version="$version_major.$version_minor.$version_maint"
 
-#version_old=$(cat "$file_name.ino" | grep " * Version:" | awk -F' ' '{print $3}')
-
-#if [[ "$version_old" == "$version" ]]; then
-#  version_maint=$((version_maint+1))
-#  echo $version_maint
-#  version="$version_major.$version_minor.$version_maint"
-#  sed -i -e "s/.*VERSION_MAINT.*/#define VERSION_MAINT $version_maint  \/\/ Maintenance version/" "version.h"
-#  rm -rf "version.h-e"
-#fi
-
 if [[ -z $version ]]; then
   echo "Error: No version string specified"
   exit 1
